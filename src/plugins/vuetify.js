4f20f17906c778e20dcff7d93487183b8329e738
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-28 21:51:40 -0300

    feat: enable dark theme and integrate MDI icon library in Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,21 @@
-/**
- * plugins/vuetify.js
- *
- * Framework documentation: https://vuetifyjs.com`
- */
-
-// Styles
+//imports
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
   },
 })
