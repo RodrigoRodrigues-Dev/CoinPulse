758ede32353225d8ae7608cfaa58a3cf88059302
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-28 22:03:55 -0300

    feat: create App component with NavBar, Header, and dynamic background based on theme

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
+    <!-- Navigation Page -->
+    <NavBar />
+
+    <!-- Header Page -->
+    <Header />
+
+    <!-- Main Page -->
+    <v-main :style="backgroundStyle">
+      <router-view class="ma-5" />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
+// Imports
+import { computed } from 'vue';
+import { useTheme } from 'vuetify';
+
+// Component Imports
+import NavBar from './components/NavBar.vue';
+import Header from './components/Header.vue';
+
+// Theme Setup
+const theme = useTheme();
+
+// Background Style Computation
+const backgroundStyle = computed(() => {
+  if (theme.global.name.value === 'dark') {
+    return 'background: linear-gradient(180deg, #111111 0%, #442e43 100%);';
+  } else {
+    return 'background: linear-gradient(180deg, #ffffff 0%, #c7a1bc 100%);';
+  }
+});
 </script>
+
+<style scoped>
+/* Additional scoped styles can go here */
+</style>
