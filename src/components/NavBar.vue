4cc018c6e035f5c84f17ca7bda9a8bed99bba885
Rodrigo Roigues <rs9844660@gmail.com>
2025-01-07 20:07:33 -0300

    feat: add login/logout functionality in NavBar with user authentication state

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,20 @@
 <template>
     <v-navigation-drawer :width="110" class="py-5 border-none" mobile-breakpoint="md" v-model="appStore.isModalVisible">
-
         <!-- Menu Logo -->
         <template v-slot:prepend>
             <v-list-item>
-                <v-img class="mx-auto" :width="50" src="/src/assets/logo.png"></v-img>
+                <v-img class="mx-auto" :width="50" src="../assets/logo.png"></v-img>
             </v-list-item>
         </template>
 
         <!-- Top Menu -->
         <RouterLink style="color: inherit;" v-for="(item, index) in menuItemsTop" :key="index"
             :to="item.name === 'Home' ? '/' : `/${item.name}`">
-            <v-list-item @click="renameSelectPage(item)" @mouseover="hoveredItem = item.name"
-                @mouseleave="hoveredItem = null" class="d-flex justify-center mt-5">
+            <v-list-item 
+                @click="renameSelectPage(item)" 
+                @mouseover="hoveredItem = item.name"
+                @mouseleave="hoveredItem = null" 
+                class="d-flex justify-center mt-5">
                 <v-icon :class="{ 'icon-hover': hoveredItem === item.name || appStore.selectPage === item.name }">
                     {{ item.icon }}
                 </v-icon>
@@ -27,32 +29,60 @@
                     <v-icon class="mb-5" color="pink-accent-3" size="20">
                         {{ themeFunction.darkMode ? 'mdi-moon-waning-crescent' : 'mdi-white-balance-sunny' }}
                     </v-icon>
-                    <v-switch v-model="themeFunction.switchActive" color="pink-accent-4"
-                        class="switch d-flex align-center justify-center" inset @click="toggleTheme" />
+                    <v-switch 
+                        v-model="themeFunction.switchActive" 
+                        color="pink-accent-4" 
+                        class="switch d-flex align-center justify-center" 
+                        inset 
+                        @click="toggleTheme" />
                 </div>
             </v-list-item>
 
-            <!-- User -->
-            <v-list-item v-for="(item, index) in menuItemsBottom" :key="index" @mouseover="hoveredItem = item.icon"
-                @mouseleave="hoveredItem = null" class="d-flex justify-center" link>
-                <v-icon :class="{ 'icon-hover': hoveredItem === item.icon }">
-                    {{ item.icon }}
+            <!-- User Menu -->
+            <RouterLink style="color: inherit;" :to="'/User'">
+                <v-list-item 
+                    @mouseover="hoveredItem = 'User'" 
+                    @mouseleave="hoveredItem = null" 
+                    class="d-flex justify-center" 
+                    link>
+                    <v-icon :class="{ 'icon-hover': hoveredItem === 'User' }">
+                        mdi-account-outline
+                    </v-icon>
+                </v-list-item>
+            </RouterLink>
+
+            <!-- Sign Out / Sign In -->
+            <v-list-item style="color: inherit;" v-if="userStore.userIsLoggedIn" class="d-flex justify-center" @click="handleSignOut" link>
+                <v-icon>
+                    mdi-logout
                 </v-icon>
             </v-list-item>
-        </template>
 
+            <RouterLink style="color: inherit;" to="/SignIn" v-if="!userStore.userIsLoggedIn">
+                <v-list-item class="d-flex justify-center" link>
+                    <v-icon>
+                        mdi-login
+                    </v-icon>
+                </v-list-item>
+            </RouterLink>
+        </template>
     </v-navigation-drawer>
 </template>
 
 <script setup>
 // Imports
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useUserStore } from '@/stores/userStore';
 import { useAppStore } from '@/stores/app';
 import { useDisplay } from 'vuetify';
 import { useTheme } from 'vuetify';
 import { useThemeFunction } from '@/stores/themeFunction';
+import { RouterLink } from 'vue-router';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import router from '@/router';
 
 // Store
+const userStore = useUserStore();
 const appStore = useAppStore();
 const themeFunction = useThemeFunction();
 const { mdAndUp } = useDisplay();
@@ -61,6 +91,22 @@ const theme = useTheme();
 // Reactive State
 const hoveredItem = ref(null);
 
+// Function to handle sign-out
+const handleSignOut = () => {
+    const auth = getAuth();
+    userStore.user = '';
+
+    signOut(auth)
+        .then(() => {
+            console.log("Logout successful");
+            router.push('/SignIn');
+        })
+        .catch((error) => {
+            console.error("Error during sign out: ", error);
+            alert("Logout failed: " + error.message);
+        });
+};
+
 // Menu items configuration
 const menuItemsTop = [
     { icon: 'mdi-earth-box', name: 'Home' },
@@ -68,11 +114,6 @@ const menuItemsTop = [
     { icon: 'mdi-finance', name: 'Graphics' },
     { icon: 'mdi-source-fork', name: 'Fork' },
     { icon: 'mdi-database-outline', name: 'DataBase' }
-];
-
-const menuItemsBottom = [
-    { icon: 'mdi-account-outline' },
-    { icon: 'mdi-login' }
 ];
 
 // Toggle Theme function
@@ -85,6 +126,11 @@ const toggleTheme = () => {
 const renameSelectPage = (item) => {
     appStore.selectPage = item.name;
 };
+
+// Initialize Authentication and set user state
+onMounted(() => {
+    userStore.initAuth();
+});
 </script>
 
 <style scoped>
