9e84c58c7af62c37e54ca30c023ba203ec80f1d2
Rodrigo Roigues <rs9844660@gmail.com>
2025-01-18 14:09:12 -0300

    feat: Add username fetching functionality to user store - Updated setUser action to call etchUsername when a user logs in, fetching the username from Firebase Realtime Database. - Added a new state property username to store the retrieved username. - Introduced the etchUsername action to retrieve the username based on the user's UID from Firebase Realtime Database. - Retained existing authentication logic and persistence functionality.

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,22 +1,36 @@
-// src/stores/userStore.js
 import { defineStore } from 'pinia';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, get } from 'firebase/database';
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    user: '',
+    user: null,
     userIsLoggedIn: null,
+    username: null,
   }),
   actions: {
     setUser(userData) {
       this.user = userData;
       this.userIsLoggedIn = !!userData;
+
+      if (userData) {
+        this.fetchUsername(userData.uid);
+      }
     },
     initAuth() {
       const auth = getAuth();
       onAuthStateChanged(auth, (currentUser) => {
         this.setUser(currentUser);
       });
+    },
+    async fetchUsername(uid) {
+      const db = getDatabase();
+      const userRef = ref(db, 'users/' + uid);
+      const snapshot = await get(userRef);
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+        this.username = userData.username;
+      }
     },
   },
   persist: true,
